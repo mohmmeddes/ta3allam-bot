f6699ee63e9cfc2ee72bae441b3592dfe5cba56b
mohmmeddes <49797454+mohmmeddes@users.noreply.github.com>
2025-06-05 02:14:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,9 +52,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 @app.route(f"/{TELEGRAM_TOKEN}", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
-    asyncio.run(app_bot.process_update(update))
+
+    async def process():
+        await app_bot.initialize()
+        await app_bot.process_update(update)
+
+    asyncio.run(process())
     return "ok"
 
+    
 @app.route('/')
 def home():
     return 'بوت تعلّم شغال ✅'
