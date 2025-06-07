bc4c07f79d786cc87b026f552fdb6eabd6099c0a
mohmmeddes <49797454+mohmmeddes@users.noreply.github.com>
2025-06-08 02:26:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-# config.py
-# ✅ تم تركيب التوكن الجديد بنجاح
-
-TOKEN = "7718520666:AAFZ4Im0AdvXmlKs0ZG9ai19QiohKMjYl10"
+# confi# 
+TELEGRAM_BOT_TOKEN = "7718520666:AAFZ4Im0AdvXmlKs0ZG9ai19QiohKMjYl10"
