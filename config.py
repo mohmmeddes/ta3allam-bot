a51075f348cd33ec06a24847becc8bea9c1eeaad
mohmmeddes <49797454+mohmmeddes@users.noreply.github.com>
2025-06-07 09:38:50 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # config.py
 import os
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+
+# توكن تليجرام - مؤقت للتجربة والتعليم
+TELEGRAM_BOT_TOKEN = "7718520666:AAH6pdCGu3T4W6KRDonEJ-Byi8a6vDUBfh8"
